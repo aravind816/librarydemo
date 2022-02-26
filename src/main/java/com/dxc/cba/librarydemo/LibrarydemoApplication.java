39de06f6313d7670d888c7b64d116eda6f02176b
aravind816 <aravind816@gmail.com>
2022-02-26 18:55:47 +1100

    Removing cluttered code.

--- src/main/java/com/dxc/cba/librarydemo/LibrarydemoApplication.java
+++ src/main/java/com/dxc/cba/librarydemo/LibrarydemoApplication.java
@@ -1,17 +1,6 @@
 package com.dxc.cba.librarydemo;
-
-import java.sql.Date;
-import java.util.Optional;
-
-import com.dxc.cba.librarydemo.model.Book;
-import com.dxc.cba.librarydemo.presistence.BookRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 
@@ -19,7 +8,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 @OpenAPIDefinition
 public class LibrarydemoApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(LibrarydemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarydemoApplication.class, args);
 	}
